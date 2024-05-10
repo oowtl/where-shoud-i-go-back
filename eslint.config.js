7e541d9f09627cbf82f8d353984b2c7af6cdcb15
Jun <oowtl7425@gmail.com>
2024-05-10 10:22:54 +0900

    fix : ts-eslint 삭제

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,9 @@
 import eslintPluginNode from 'eslint-plugin-node';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default [
   eslintJs.configs.recommended,
-  ...tseslint.configs.recommended,
   eslintPluginNode.configs.recommended,
   eslintConfigPrettier,
-);
+];
